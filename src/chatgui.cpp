9b510f354ff73f2057e4203e8818a9447e54db27
root <lennartsteinhoff@gmx.de>
2023-03-09 14:08:51 +0000

    fix unique ptr init

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -120,7 +120,7 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = std::make_unique<ChatLogic>(ChatLogic()); 
+    _chatLogic = std::unique_ptr<ChatLogic>(new ChatLogic()); 
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
